36dc9f04f5021ca9195f70baeae1db42b5ea6260
Harrison Stropkay <harrison.f.stropkay.25@dartmouth.edu>
2024-05-08 13:20:11 -0400

    more reorganize

--- collate_data/swap_columns.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("authors_nltk.csv")
-
-# swap to author,prompt,response,common_vector,function_vector
-df = df[["author", "prompt", "response", "common_vector", "function_vector"]]
-
-df.to_csv("authors_final.csv", index=False)
